0e9e762d2834e01334ffb864d69f0f8c21307a19
Sheyranyann <139764356+Sheyranyann@users.noreply.github.com>
2023-07-30 12:45:42 +0400

    Update DigitalProduct.java

--- DigitalProduct.java
+++ DigitalProduct.java
@@ -1,24 +1,13 @@
 public class DigitalProduct extends Product {
-    private final String name;
-    private final Float price;
     private final Float size;
 
     public DigitalProduct(String name, Float price, Float size) {
-        this.name = name;
-        this.price = price;
+        super(name, price);
         this.size = size;
     }
 
     public Float getDownloadSize() {
          return size;
      }
-    @Override
-    public String getName() {
-        return name;
-    }
-    @Override
-    public Float getPrice() {
-        return price;
-    }
 
 }
