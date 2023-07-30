73fd400c85abbd625a304e0aca08da8e5116e6e2
Sheyranyann <139764356+Sheyranyann@users.noreply.github.com>
2023-07-30 12:46:14 +0400

    Update PhysicalProduct.java

--- PhysicalProduct.java
+++ PhysicalProduct.java
@@ -1,10 +1,8 @@
 public class PhysicalProduct extends Product {
-    private final String name;
-    private final Float price;
     private final Float weight;
+
     public PhysicalProduct(String name, Float price, Float weight) {
-        this.name = name;
-        this.price = price;
+        super(name, price);
         this.weight = weight;
     }
 
@@ -12,13 +10,4 @@ public class PhysicalProduct extends Product {
         return weight;
     }
 
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public Float getPrice() {
-        return price;
-    }
 }
